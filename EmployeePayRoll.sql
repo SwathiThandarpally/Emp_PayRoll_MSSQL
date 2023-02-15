fa55cc3eb6fb5e5110e2188a82e93f58df17b596
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-15 23:16:09 +0530

    Added Create Table Employee_payroll

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -2,3 +2,12 @@
 
 --UseCase1-Create a Pay_RollService Database------
 create database Pay_RollService
+
+--UseCase2- Creating a Table
+Create Table employee_payroll(
+Id int PRIMARY KEY IDENTITY(1,1),
+Name varchar(30) NOT NULL,
+Salary BIGINT NOT NULL,
+Start DATE NOT NULL
+);
+select * from employee_payroll;
