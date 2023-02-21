40442fbe62b435c0b6ed5d6d5b68cb832fb41419
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-21 22:49:55 +0530

    Added Gender Details To The Emp_payroll

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -12,14 +12,21 @@ Start DATE NOT NULL
 );
 
 --UseCase3- create employee_payroll data in the Pay_RollService database
-INSERT INTO Employee_Payroll(Name,Salary,Start)VALUES
+INSERT INTO employee_payroll(Name,Salary,Start)VALUES
 ('Kairali',15000,'2012-02-23'),
 ('Moksha',20000,'2010-09-22'),
 ('Tommy',17500,'2011-08-12');
+
  
 --UseCase-4 retrieve all the employee payroll
 select * from employee_payroll;
 
---UC5 Get Data from particular range
+--UseCase-5 Get Data from particular range
 SELECT Salary FROM employee_payroll WHERE Name='Tommy';
 SELECT * FROM employee_payroll WHERE Start BETWEEN CAST('2011-08-12' AS DATE) AND GETDATE();
+
+--UseCase-6 Add Gender to Employee Payroll Table
+ALTER TABLE employee_payroll ADD Gender varchar(10)
+UPDATE employee_payroll SET Gender ='Female' WHERE Name='Kairali'
+UPDATE employee_payroll SET Gender='Female' WHERE Name='Moksha'
+UPDATE employee_payroll SET Gender='Male' WHERE Name='Tommy'
