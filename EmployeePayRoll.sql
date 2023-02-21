b85d7a1ae64eae0c43e40b820c45b85a34799244
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-02-21 22:38:18 +0530

    Added Get Data From Particular Range

--- EmployeePayRoll.sql
+++ EmployeePayRoll.sql
@@ -10,14 +10,16 @@ Name varchar(30) NOT NULL,
 Salary BIGINT NOT NULL,
 Start DATE NOT NULL
 );
-select * from employee_payroll;
 
 --UseCase3- create employee_payroll data in the Pay_RollService database
 INSERT INTO Employee_Payroll(Name,Salary,Start)VALUES
 ('Kairali',15000,'2012-02-23'),
 ('Moksha',20000,'2010-09-22'),
 ('Tommy',17500,'2011-08-12');
-select * from employee_payroll;
  
 --UseCase-4 retrieve all the employee payroll
 select * from employee_payroll;
+
+--UC5 Get Data from particular range
+SELECT Salary FROM employee_payroll WHERE Name='Tommy';
+SELECT * FROM employee_payroll WHERE Start BETWEEN CAST('2011-08-12' AS DATE) AND GETDATE();
